f27593c878d07065e83ade4f1c0054d04960fcea
XimenaEne <57491810+XimenaEne@users.noreply.github.com>
2019-11-09 19:49:38 +0200

    Mesaj serios!

--- v-offer/src/router/routes.js
+++ v-offer/src/router/routes.js
@@ -3,7 +3,7 @@ const routes = [
     path: "/",
     component: () => import("layouts/MyLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/Index.vue") },
+      { path: "/", component: () => import("pages/Index.vue") },
       { path: "/profile", component: () => import("pages/Profile.vue") }
     ]
   },
